1aec90b55f708f62794155038834be94ace05d96
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2023-02-10 12:20:28 +0100

    Added setting sensor time intervals

--- src/libtm.c
+++ src/libtm.c
@@ -3,27 +3,45 @@
 // #include <fcntl.h>
 // #include <stdio.h>
 // #include <sys/stat.h>
-// #include <sys/types.h>
 // #include <unistd.h>
 // // #include <macros.h> // Framsat .h
 // #include <errno.h>
 // #include <string.h>
 // // #include <libtiming.h>
 // #include <stdlib.h>
+// #include <sys/types.h>
 
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "../inc/libtm.h"
 
-#define DO_DEBUG 1
+#define DO_DEBUG 0
 
 #define ArrayLength(Array) (sizeof(Array) / sizeof(Array[0]))
 #ifdef DO_DEBUG
 #define DEBUG(string) string
 #endif
 
+// TODO(ingar): give this struct a better name since it's used for multiple
+// things
+//  2 * 9 = 18  bytes
+struct __attribute__((__packed__)) sensor_write_intervals_ms
+{
+    uint16_t IMU_Mean_Gyro_XYZ;
+    uint16_t IMU_Mean_Magnetometer;
+    uint16_t Controller_output_raw;
+    uint16_t Controller_output_to_imtq;
+    uint16_t envModels_Mag_field;
+    uint16_t envModels_Sun_position;
+    uint16_t envModels_radius_ECI;
+    uint16_t envModels_quarternion_ECI;
+    uint16_t iMTQ_housekeeping;
+};
+
+// 3 * 4 = 12 bytes
 struct __attribute__((__packed__)) vec3
 {
     float x;
@@ -31,6 +49,7 @@ struct __attribute__((__packed__)) vec3
     float z;
 };
 
+// 11 * 2 = 22 bytes
 struct __attribute__((__packed__)) iMTQ_housekeeping_data
 {
     uint16_t dig_volt;    /**< Measured voltage of digital supply  */
@@ -46,6 +65,7 @@ struct __attribute__((__packed__)) iMTQ_housekeeping_data
     int16_t MCU_temp;     /**< Temperature measurement of the MCU  */
 };
 
+// 12 * 8 + 22 = 118 bytes
 struct __attribute__((__packed__)) ADCS_sensor_data
 {
     struct vec3 IMU_Mean_Gyro_XYZ;
@@ -60,14 +80,59 @@ struct __attribute__((__packed__)) ADCS_sensor_data
 };
 
 static struct ADCS_sensor_data _latest_polled_data;
+static struct sensor_write_intervals_ms _sensor_write_intervals_ms;
+static struct sensor_write_intervals_ms
+    _elapsed_time_since_last_sensor_write_ms;
+
+void libtm_write_to_file()
+{
+    struct timeval timeval_current_time;
+    gettimeofday(&timeval_current_time, NULL);
+
+    uint8_t sensors_to_write[N_SENSORS] = {0};
+    uint8_t n_sensors_to_write = 0;
+    uint16_t *sensor_write_intervals = (uint16_t *)&_sensor_write_intervals_ms;
+    uint16_t *elapsed_time_since_last_sensor_write =
+        (uint16_t *)&_elapsed_time_since_last_sensor_write_ms;
+
+    for (int i = 0; i < N_SENSORS; ++i)
+    {
+        elapsed_time_since_last_sensor_write[i] +=
+            timeval_current_time.tv_usec * 1000;
+
+        if ((sensor_write_intervals[i] == 0) ||
+            (elapsed_time_since_last_sensor_write[i] < sensor_write_intervals[i]))
+        {
+            continue;
+        }
+
+        
+    }
+}
+
+void libtm_set_sensor_write_interval(uint16_t interval_ms, uint8_t sensor_id)
+{
+    if (sensor_id < 1 || sensor_id > N_SENSORS)
+    {
+        DEBUG(printf("Invalid sensor id when setting sensor write interval; "
+                     "expected a value between 1 and %d "
+                     "inclusive, was actually %d\n",
+                     N_SENSORS, sensor_id);)
+        return;
+    }
+
+    uint16_t *sensor_write_intervals = (uint16_t *)&_sensor_write_intervals_ms;
+    sensor_write_intervals[sensor_id - 1] = interval_ms;
+}
 
 int libtm_set_sensor_data(float x, float y, float z, uint8_t sensor_id)
 {
-    if (sensor_id >= IMTQ_HOUSEKEEPING)
+    if (sensor_id < 1 || sensor_id >= IMTQ_HOUSEKEEPING)
     {
-        DEBUG(printf("Invalid sensor id; expected a value between 1 and %d "
+        DEBUG(printf("Invalid sensor id when setting sensor data; expected a value "
+                     "between 1 and %d "
                      "inclusive, was actually %d\n",
-                     N_SENSORS + 1, IMTQ_HOUSEKEEPING - 1);)
+                     IMTQ_HOUSEKEEPING - 1, sensor_id);)
         return -1;
     }
 
@@ -78,37 +143,21 @@ int libtm_set_sensor_data(float x, float y, float z, uint8_t sensor_id)
     sensor_data[sensor_id - 1].z = z;
 }
 
+// NOTE(ingar): Should the byte be sent in separately from the buffer.
 void libtm_set_iMTQ_housekeeping_data(const void *iMTQ_housekeeping_data)
 {
-    // printf("Address of envModels_quarternion_ECI: %p\n", &_latest_polled_data.envModels_quarternion_ECI);
-    // printf("Address of imtq: %p\n", &_latest_polled_data.iMTQ_housekeeping);
-    // printf("Diff: %ld\n", (uint64_t)&_latest_polled_data.iMTQ_housekeeping - (uint64_t)&_latest_polled_data.envModels_quarternion_ECI);
     memcpy((void *)&_latest_polled_data.iMTQ_housekeeping, iMTQ_housekeeping_data,
            sizeof(struct iMTQ_housekeeping_data));
 }
 
-void print_ADCS_data()
+static void libtm_run_tests();
+static void print_ADCS_data();
+
+int main()
 {
-    struct vec3 *sensor_data = (struct vec3 *)&_latest_polled_data;
-    for (int i = 0; i < IMTQ_HOUSEKEEPING - 1; ++i)
-    {
-        printf("Sensor %d values: X=%f Y=%f Z=%f\n", i, sensor_data[i].x, sensor_data[i].y, sensor_data[i].z);
-    }
+    // libtm_run_tests();
 
-    printf("\n");
-
-    uint16_t *imtq_data = (uint16_t *)&_latest_polled_data.iMTQ_housekeeping;
-    for (int i = 0; i < 11; ++i)
-    {
-        if (i < 4)
-        {
-            printf("Field %d values: %d\n", i, imtq_data[i]);
-        }
-        else
-        {
-            printf("Field %d values: %d\n", i, (int16_t)imtq_data[i]);
-        }
-    }
+    return 0;
 }
 
 static void libtm_run_tests()
@@ -128,6 +177,7 @@ static void libtm_run_tests()
 
     libtm_set_iMTQ_housekeeping_data((const void *)&iMTQ_housekeeping_data);
 
+    libtm_set_sensor_data(1, 2, 3, 0);
     libtm_set_sensor_data(1, 2, 3, 1);
     libtm_set_sensor_data(4, 5, 6, 2);
     libtm_set_sensor_data(7, 8, 9, 3);
@@ -136,10 +186,10 @@ static void libtm_run_tests()
     libtm_set_sensor_data(16, 17, 18, 6);
     libtm_set_sensor_data(19, 20, 21, 7);
     libtm_set_sensor_data(22, 23, 24, 8);
+    libtm_set_sensor_data(1, 2, 3, 9);
+    // print_ADCS_data();
 
-    print_ADCS_data();
-
-    printf("\n");
+    DEBUG(printf("\n");)
 
     uint16_t new_imtq_data[11] = {12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
     libtm_set_iMTQ_housekeeping_data((const void *)new_imtq_data);
@@ -153,12 +203,45 @@ static void libtm_run_tests()
     libtm_set_sensor_data(40, 41, 42, 7);
     libtm_set_sensor_data(43, 44, 45, 8);
 
-    print_ADCS_data();
+    // print_ADCS_data();
+
+    libtm_set_sensor_write_interval(50, 0);
+    libtm_set_sensor_write_interval(50, 10);
+    for (int i = 1; i <= N_SENSORS; ++i)
+    {
+
+        libtm_set_sensor_write_interval(50 * i, i);
+    }
+
+    uint16_t *sensor_write_intervals = (uint16_t *)&_sensor_write_intervals_ms;
+    for (int i = 0; i < N_SENSORS; ++i)
+    {
+        DEBUG(printf("Sensor %d write interval: %d\n", i + 1,
+                     sensor_write_intervals[i]);)
+    }
 }
 
-int main()
+static void print_ADCS_data()
 {
-    libtm_run_tests();
+    struct vec3 *sensor_data = (struct vec3 *)&_latest_polled_data;
+    for (int i = 0; i < IMTQ_HOUSEKEEPING - 1; ++i)
+    {
+        DEBUG(printf("Sensor %d values: X=%f Y=%f Z=%f\n", i + 1, sensor_data[i].x,
+                     sensor_data[i].y, sensor_data[i].z);)
+    }
 
-    return 0;
+    DEBUG(printf("\n");)
+
+    uint16_t *imtq_data = (uint16_t *)&_latest_polled_data.iMTQ_housekeeping;
+    for (int i = 0; i < 11; ++i)
+    {
+        if (i < 4)
+        {
+            DEBUG(printf("Field %d values: %d\n", i + 1, imtq_data[i]);)
+        }
+        else
+        {
+            DEBUG(printf("Field %d values: %d\n", i + 1, (int16_t)imtq_data[i]);)
+        }
+    }
 }
